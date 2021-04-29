45aaf9dfdcadf849b7de34fe9ef1cbd85418e855
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-29 20:24:21 +0530

    Add files via upload

---
+++ Largestof3numbers.java
@@ -0,0 +1,31 @@
+package ultracode;
+
+public class Largestof3numbers {
+
+	public static void main(String[] args) {
+		
+		 int num1 = 36, num2 = 35, num3 = 56;
+	      if(num1 >= num2) 
+	      {
+	         if(num1 >= num3)
+	         {
+	            System.out.println(num1 + " is largest number.");
+	         }
+	         else
+	         {
+	            System.out.println(num3 + " is largest number.");
+	         }
+	      } 
+	      else 
+	      {
+	         if(num2 >= num3)
+	         {
+	            System.out.println(num2 + " is largest number.");
+	         }
+	         else
+	         {
+	            System.out.println(num3 + " is largest number.");
+	         }
+	      }
+	   }
+	}
