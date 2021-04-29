8b3f5c8cb3d6b53ac94958c1bd58b20450065c42
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-29 20:22:42 +0530

    Add files via upload

---
+++ SortStringArray.java
@@ -0,0 +1,26 @@
+package ultracode;
+import java.util.Arrays;
+
+public class SortStringArray {
+
+	public static void main(String[] args) {
+
+		String[] my_array = {
+	            "Praveen",
+				
+	            "Shiva",
+	            "Rajesh",
+	            "Dinki",
+	            "Pranay",
+	            "Siridi",
+	            "Yaswanth",
+	            "Mom",
+	            "Dad"
+	        }; 
+		System.out.println("Original string array : "+Arrays.toString(my_array));
+	    Arrays.sort(my_array);
+	    System.out.println("Sorted string array : "+Arrays.toString(my_array));
+		
+	}
+
+}
