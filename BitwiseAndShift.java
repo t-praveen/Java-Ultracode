f8e229eebb33e87729d8550a16723542ab8cbb50
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-29 20:25:02 +0530

    Add files via upload

---
+++ BitwiseAndShift.java
@@ -0,0 +1,18 @@
+package ultracode;
+
+public class BitwiseAndShift {
+
+	public static void main(String[] args) {
+		
+		int x = 9, y = 8; 
+		int a = 50 ;
+		System.out.println("x & y = " + (x & y)); 
+		System.out.println("x ^ y = " + (x ^ y)); 
+		System.out.println("x | y = " + (x | y));   
+		System.out.println("a>>2 = " + (a >>2));
+		System.out.println("a<<1 = " + (a << 1));
+		
+
+	}
+
+}
